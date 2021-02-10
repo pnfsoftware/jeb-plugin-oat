b9c077f6114120b6e08ce94e33c3b092fac31165
Nicolas Falliere <nico@pnfsoftware.com>
2021-02-10 15:27:19 -0800

    Minor update for JEB4 compatibility

--- build-windows.cmd
+++ build-windows.cmd
@@ -1,2 +1,2 @@
 @echo off
-ant -f scripts\build.xml -DpluginClassname=com.pnf.plugin.oat.OATPlugin -DpluginFilename=JebOatPlugin -DpluginVersion=1.0.8
+ant -f scripts\build.xml -DpluginClassname=com.pnf.plugin.oat.OATPlugin -DpluginFilename=JebOatPlugin -DpluginVersion=1.0.9
